5c022bdb5db950948fd9b5280b941065f6275b78
wesleysmits <wesley@intermation.nl>
2021-07-11 16:59:11 +0200

    Added a template for full-page and almost-full-page pages

---
+++ wp-content/themes/lavandre/template-parts/almost-full-page.php
@@ -0,0 +1,22 @@
+<?php
+    /* Template Name: Almost-full-page Template */
+
+    if ( ! defined( 'ABSPATH' ) ) {
+        exit; // Exit if accessed directly.
+    }
+    get_header();
+?>
+<?php
+while ( have_posts() ) : the_post();
+	?>
+
+    <main <?php post_class( 'site-main ww-container' ); ?> role="main">
+        <div class="page-content">
+            <?php the_content(); ?>
+        </div>
+    </main>
+
+	<?php
+endwhile;
+
+get_footer();
