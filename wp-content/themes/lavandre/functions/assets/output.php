46e1c48707b10c924b4af7d4e514e52026ef753b
Wesley Smits <wesley@intermation.nl>
2021-07-25 10:45:11 +0200

    Improve asset loading (#25)
    * Added jQuery
    
    * Fixed asset loading
    
    * Fixed asset loading
    
    * Upgrade Cypress
    
    * Fix

---
+++ wp-content/themes/lavandre/functions/assets/output.php
@@ -0,0 +1,20 @@
+<?php
+    function add_rel_preload($html, $handle, $href, $media) {
+        if (is_admin()) {
+            return $html;
+        }
+
+        return <<<EOT
+            <link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" href='$href' type='text/css' media='all' />
+            EOT;
+    }
+    add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
+
+    function add_type_module($html, $handle, $src) {
+        if (is_admin() || strpos($html, 'lavandre/') === false) {
+            return $html;
+        }
+
+        return str_replace("text/javascript", "module", $html);
+    }
+    add_filter( 'script_loader_tag', 'add_type_module', 10, 3 );
---
+++ wp-content/themes/lavandre/functions/assets/utils.php
@@ -0,0 +1,29 @@
+<?php
+    /* ENCORE MODULE METHODS */
+    function loadEncoreModule(string $moduleName) {
+        $webpackEncore = WebpackEncore::getInstance();
+
+        if (!$webpackEncore) {
+            return;
+        }
+
+        $webpackEncore->enqueue_entry_css($moduleName, false);
+        $webpackEncore->enqueue_entry_js($moduleName, false);
+    }
+
+    /* IMAGE KIT METHODS */
+    function get_image_kit_url(string $src) {
+        $baseUrl = get_site_url();
+        $newSrc = str_replace($baseUrl, 'https://ik.imagekit.io/gw83zqxvr9d/', $src);
+        return $newSrc;
+    }
+
+    function get_image_kit_placeholder($src, $width = 400, $height = 300) {
+        $url = get_image_kit_url($src);
+        return $url . '?tr=w-1,h-1:w-' . $width . ',h-' . $height;
+    }
+
+    function get_image_kit_src($src, $width = 400, $height = 300) {
+        $url = get_image_kit_url($src);
+        return $url . '?tr=w-' . $width . ',h-' . $height;
+    }
