ced59047d4b999aae2e30b756728810c207d0a09
wesleysmits <wesley@intermation.nl>
2021-07-25 08:26:53 +0200

    Fixed asset loading

--- wp-content/themes/lavandre/functions/assets.php
+++ wp-content/themes/lavandre/functions/assets.php
@@ -1,109 +1,113 @@
 <?php
-
-// Load translation files from your child theme instead of the parent theme
-function my_child_theme_locale() {
+add_action( 'after_setup_theme', function() {
+    // Load translation files from your child theme instead of the parent theme
     load_child_theme_textdomain( 'total', get_stylesheet_directory() . '/languages' );
     load_child_theme_textdomain( 'lavandre', get_stylesheet_directory() . '/languages' );
     load_child_theme_textdomain( 'hello-elementor-child', get_stylesheet_directory() . '/languages' );
-}
-add_action( 'after_setup_theme', 'my_child_theme_locale' );
 
-/**
- * Remove Gutenberg Block Library CSS from loading on the frontend
- */
-function smartwp_remove_wp_block_library_css(){
+    // Remove woo three image gallery support
+    remove_theme_support( 'wc-product-gallery-zoom' );
+    remove_theme_support( 'wc-product-gallery-slider' );
+    remove_theme_support( 'wc-product-gallery-lightbox' );
+} );
+
+add_action( 'wp_footer', function() {
+    wp_deregister_script( 'wp-embed' );
+    wp_dequeue_script('wc-add-to-cart-variation');
+} );
+
+add_action( 'wp_enqueue_scripts', function () {
+    wp_enqueue_scripts_lavandre();
+    wp_enqueue_styles_lavandre();
+    loadWebpackModules();
+
+    /* Output enqueued scripts   */
+    global $wp_scripts;
+    global $enqueued_scripts;
+    $enqueued_scripts = array();
+    foreach( $wp_scripts->queue as $handle ) {
+        $enqueued_scripts[] = $handle." | ".$wp_scripts->registered[$handle]->src;
+    }
+    var_dump($enqueued_scripts);
+    // die;
+
+    /* Output enqueued styles */
+    global $wp_styles;
+    foreach( $wp_styles->queue as $handle ) :
+        var_dump($handle);
+    endforeach;
+    // die;
+}, 1000 );
+
+function wp_enqueue_scripts_lavandre() {
+    // Remove Gutenberg Block Library CSS from loading on the frontend
     wp_dequeue_style( 'wp-block-library' );
     wp_dequeue_style( 'wp-block-library-theme' );
     wp_dequeue_style( 'wc-block-style' );
-}
-add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
 
-// add_action( 'wp_enqueue_scripts', function() { wp_dequeue_style( 'font-awesome' ); }, 50 );
-
-add_action( 'wp_enqueue_scripts', 'crunchify_enqueue_scripts_styles' );
-function crunchify_enqueue_scripts_styles() {
-     wp_dequeue_script( 'devicepx' );
-}
-add_filter( 'jetpack_implode_frontend_css', '__return_false', 99 );
-add_filter( 'jetpack_sharing_counts', '__return_false', 99 );
-
-add_action( 'wp_enqueue_scripts', 'remove_woocommerce_generator', 99 );
-function remove_woocommerce_generator() {
-    if (function_exists( 'is_woocommerce' )) {
-        if (!is_woocommerce()) {
-            remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
-        }
-    }
-}
-
-add_action( 'wp_print_scripts', 'woocommerce_de_script', 100 );
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
-function woocommerce_de_script() {
-    wp_dequeue_script('bodhi_svg_inline');
+    // Remove TranslatePress Front-end code
     wp_dequeue_script('trp-frontend-compatibility');
 
+    // Remove SVG front-end code
+    wp_dequeue_script('bodhi_svg_inline');
+
+    // Remove jetpack devicepx
+    wp_dequeue_script( 'devicepx' );
+
+    // If Woocommerce exists
     if (function_exists( 'is_woocommerce' )) {
-        if (!is_woocommerce() && !is_cart() && !is_account_page() ) {
-            wp_dequeue_script('wc-add-to-cart');
-            wp_dequeue_script('jquery-blockui');
-            wp_dequeue_script('jquery-placeholder');
-            wp_dequeue_script('woocommerce');
-            wp_dequeue_script('jquery-cookie');
-            wp_dequeue_script('wc-cart-fragments');
-            wp_dequeue_script('wpm_product_gtin_frontend');
-            wp_dequeue_script('mailchimp-woocommerce');
-        }
-    }
-
-    if (is_shop()) {
-        wp_dequeue_script('wc-add-to-cart');
-        wp_dequeue_script('woocommerce');
-        wp_dequeue_script('wc-cart-fragments');
-    }
-
-    if (is_product()) {
+        // Global woocommerce dequeues
         wp_dequeue_script('wc-add-to-cart');
         wp_dequeue_script('jquery-blockui');
         wp_dequeue_script('jquery-placeholder');
         wp_dequeue_script('woocommerce');
         wp_dequeue_script('jquery-cookie');
         wp_dequeue_script('wc-cart-fragments');
-        wp_dequeue_script('zoom');
-        wp_dequeue_script('flexslider');
-        wp_dequeue_script('photoswipe-ui-default');
-        wp_dequeue_script('wc-single-product');
-    }
+        wp_dequeue_script('selectWoo');
+        wp_deregister_script('selectWoo');
 
-    if (is_front_page()) {
-        // wp_dequeue_script('smartmenus'); // neccessary for mobile menu dropdown and possibly accordeons?
+        // Remove product search assets
         wp_dequeue_script('typewatch');
         wp_dequeue_script('product-search');
+
+        if (!is_woocommerce() && !is_cart() && !is_account_page() ) {
+            wp_dequeue_script('mailchimp-woocommerce');
+
+            // Remove woocommerce generator
+            remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
+        }
+
+        if (is_product()) {
+            wp_dequeue_script('zoom');
+            wp_dequeue_script('flexslider');
+            wp_dequeue_script('photoswipe-ui-default');
+            wp_dequeue_script('wc-single-product');
+        }
     }
-
-    // global $wp_scripts;
-    // global $enqueued_scripts;
-    // $enqueued_scripts = array();
-    // foreach( $wp_scripts->queue as $handle ) {
-    //     $enqueued_scripts[] = $handle." | ".$wp_scripts->registered[$handle]->src;
-    // }
-    // var_dump($enqueued_scripts);
-    // die;
 }
+// add_action( 'wp_enqueue_scripts', 'wp_enqueue_scripts_lavandre', 100 );
+add_filter( 'jetpack_implode_frontend_css', '__return_false', 99 );
+add_filter( 'jetpack_sharing_counts', '__return_false', 99 );
 
-add_action( 'wp_enqueue_scripts', 'woocommerce_de_style', 99999999 );
-function woocommerce_de_style() {
+// add_action( 'wp_enqueue_scripts', 'woocommerce_de_style', 99999999 );
+function wp_enqueue_styles_lavandre() {
     // wp_dequeue_style('affwp-forms');
+    wp_dequeue_style('wpforms-full');
+    wp_dequeue_style('trp-language-switcher-style');
+    wp_dequeue_style('wc-block-vendors-style');
+    wp_dequeue_style('product-search');
+    wp_dequeue_style('wcsatt-css');
 
-    // WPForms Contact page
-    // if (!is_page(245)) {
-        wp_dequeue_style('wpforms-full');
-    // }
+    wp_dequeue_style('wp-mediaelement');
 
     if (function_exists( 'is_woocommerce' )) {
+        if (is_woocommerce()) {
+            wp_dequeue_style('select2');
+            wp_deregister_style('select2');
+        }
+
         if (
-            (!is_woocommerce()
-            && !is_cart()
-            && !is_page(6126))
+            (!is_woocommerce() && !is_cart())
             || is_shop()
             || is_product()
         ) {
@@ -117,99 +121,13 @@ function woocommerce_de_style() {
             wp_dequeue_style('wc-bundle-style');
         }
     }
-
-    wp_dequeue_style('trp-language-switcher-style');
-
-    if (is_front_page()) {
-        wp_dequeue_style('wc-block-vendors-style');
-        wp_dequeue_style('product-search');
-        wp_dequeue_style('wcsatt-css');
-    }
-
-    wp_dequeue_style('hello-elementor-theme-style');
-
-    wp_dequeue_style('wp-mediaelement');
-
-    // global $wp_styles;
-    // foreach( $wp_styles->queue as $handle ) :
-    //     var_dump($handle);
-    // endforeach;
-    // die;
 }
 
-/**
- * Load child theme css and optional scripts
- *
- * @return void
- */
-function hello_elementor_child_enqueue_scripts() {
-    wp_dequeue_style('hello-elementor');
+function loadWebpackModules(): void {
+    // Load lavandre styles
+	wp_enqueue_style('lavandre', get_stylesheet_directory_uri() . '/style.css', [], false);
 
-	wp_enqueue_style(
-		'hello-elementor-child-style',
-		get_stylesheet_directory_uri() . '/style.css',
-		[],
-		false
-	);
-}
-
-
-function add_rel_preload($html, $handle, $href, $media) {
-    if (is_admin()) {
-        return $html;
-    }
-
-    return <<<EOT
-        <link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" href='$href' type='text/css' media='all' />
-        EOT;
-}
-add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
-
-function add_type_module($html, $handle, $src) {
-    if (is_admin() || strpos($html, 'lavandre/') === false) {
-        return $html;
-    }
-
-    return str_replace("text/javascript", "module", $html);
-}
-add_filter( 'script_loader_tag', 'add_type_module', 10, 3 );
-
-
-/**
- * Disable the emoji's
- */
-function disable_emojis() {
-	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	remove_action( 'admin_print_styles', 'print_emoji_styles' );
-	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-
-	// Remove from TinyMCE
-	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
-}
-add_action( 'init', 'disable_emojis' );
-
-/**
- * Filter out the tinymce emoji plugin.
- */
-function disable_emojis_tinymce( $plugins ) {
-	if ( is_array( $plugins ) ) {
-		return array_diff( $plugins, array( 'wpemoji' ) );
-	} else {
-		return array();
-	}
-}
-
-function my_deregister_scripts(){
-    wp_deregister_script( 'wp-embed' );
-    wp_dequeue_script('wc-add-to-cart-variation');
-}
-add_action( 'wp_footer', 'my_deregister_scripts' );
-
-add_action('wp_enqueue_scripts', function() {
+    // Load app module
     loadEncoreModule('app');
 
     if (is_front_page()) loadEncoreModule('homepage-styles');
@@ -220,6 +138,7 @@ add_action('wp_enqueue_scripts', function() {
     if (is_account_page() && is_user_logged_in()) loadEncoreModule('account-styles');
     if (is_checkout()) loadEncoreModule('checkout-styles');
 
+    // #TODO: Get id's somehow
     if (
         (is_account_page() && !is_user_logged_in())
         || is_page(317)
@@ -227,57 +146,8 @@ add_action('wp_enqueue_scripts', function() {
     ) {
         loadEncoreModule('login-styles');
     }
-});
-
-function loadEncoreModule(string $moduleName) {
-    $webpackEncore = WebpackEncore::getInstance();
-
-    if (!$webpackEncore) {
-        return;
-    }
-
-    $webpackEncore->enqueue_entry_css($moduleName, false);
-    $webpackEncore->enqueue_entry_js($moduleName, false);
 }
 
-/**
- * Get image kit URL
- */
-function get_image_kit_url(string $src) {
-    $baseUrl = get_site_url();
-    $newSrc = str_replace($baseUrl, 'https://ik.imagekit.io/gw83zqxvr9d/', $src);
-    return $newSrc;
-}
-
-function get_image_kit_placeholder($src, $width = 400, $height = 300) {
-    $url = get_image_kit_url($src);
-    return $url . '?tr=w-1,h-1:w-' . $width . ',h-' . $height;
-}
-
-function get_image_kit_src($src, $width = 400, $height = 300) {
-    $url = get_image_kit_url($src);
-    return $url . '?tr=w-' . $width . ',h-' . $height;
-}
-
-function wc_disable_select2() {
-    if ( class_exists('woocommerce') ) {
-        wp_dequeue_style('select2');
-        wp_deregister_style('select2');
-
-        // WooCommerce 3.2.1.x and below
-        wp_dequeue_script('select2');
-        wp_deregister_script('select2');
-
-        // WooCommerce 3.2.1+
-        wp_dequeue_script('selectWoo');
-        wp_deregister_script('selectWoo');
-    }
-}
-add_action('wp_enqueue_scripts', 'wc_disable_select2', 100);
-
-add_action( 'after_setup_theme', 'remove_woo_three_support', 11 );
-function remove_woo_three_support() {
-  remove_theme_support( 'wc-product-gallery-zoom' );
-  remove_theme_support( 'wc-product-gallery-slider' );
-  remove_theme_support( 'wc-product-gallery-lightbox' );
-}
+include(get_stylesheet_directory() . '/functions/assets/disable-emoji.php');
+include(get_stylesheet_directory() . '/functions/assets/utils.php');
+include(get_stylesheet_directory() . '/functions/assets/output.php');
