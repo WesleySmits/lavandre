6b3ac2c4f4a319d8375d216a6f5d9326dfdf2802
wesleysmits <wesley@intermation.nl>
2021-08-01 14:32:32 +0200

    Styling

--- wp-content/themes/lavandre/template-parts/single.php
+++ wp-content/themes/lavandre/template-parts/single.php
@@ -20,7 +20,37 @@ while ( have_posts() ) : the_post();
 		</header>
 	<?php endif; ?>
 	<div class="page-content">
-		<?php the_content(); ?>
+        <?php
+            if( have_rows('accordion') ):
+                $index = 0;
+                ?><ul class="accordion-list accordion-list--legal"><?php
+                while( have_rows('accordion') ) : the_row();
+                    $title = get_sub_field('title');
+                    $description = get_sub_field('description');
+                    $id = "page-accordion-" . $index;
+
+                    ?>
+                        <li>
+                            <article>
+                                <header>
+                                    <h3 data-curtain-toggle="<?php echo $id; ?>"><?php echo $title; ?></h3>
+                                </header>
+
+                                <div data-curtain-content="<?php echo $id; ?>">
+                                    <?php echo $description; ?>
+                                </div>
+                            </article>
+                        </li>
+                    <?php
+
+                    $index++;
+                endwhile;
+
+                ?></ul><?php
+            endif;
+        ?>
+
+        <?php the_content(); ?>
 		<div class="post-tags">
 			<?php the_tags( '<span class="tag-links">' . __( 'Tagged ', 'hello-elementor' ), null, '</span>' ); ?>
 		</div>
