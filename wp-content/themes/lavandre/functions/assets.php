5ff9523c760902f453ab8dcaa8bb481b13b0c93b
wesleysmits <wesley@intermation.nl>
2021-08-22 17:46:30 +0200

    [LC-23] Set up LiveChat widget

--- wp-content/themes/lavandre/functions/assets.php
+++ wp-content/themes/lavandre/functions/assets.php
@@ -44,6 +44,7 @@ function wp_enqueue_scripts_lavandre() {
     wp_dequeue_style( 'wp-block-library' );
     wp_dequeue_style( 'wp-block-library-theme' );
     wp_dequeue_style( 'wc-block-style' );
+    wp_dequeue_style( 'wc-blocks-style' );
 
     // Remove TranslatePress Front-end code
     wp_dequeue_script('trp-frontend-compatibility');
