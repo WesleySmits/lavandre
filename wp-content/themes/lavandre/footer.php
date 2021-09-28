ccf3d71e948b77a33dd06638aff23dc95b295cf6
wesleysmits <wesley@intermation.nl>
2021-09-28 20:02:41 +0200

    Styling fixes

--- wp-content/themes/lavandre/footer.php
+++ wp-content/themes/lavandre/footer.php
@@ -102,7 +102,7 @@
         <ul class="legal-list no-list inline-list">
             <li class="legal-list__item"><a class="legal-list__anchor" href="/terms-and-conditions/"><?php _e('Terms of use', 'lavandre'); ?></a></li>
             <li class="legal-list__item"><a class="legal-list__anchor" href="/privacy-policy/"><?php _e('Privacy policy', 'lavandre'); ?></a></li>
-            <li class="legal-list__item"><span>&copy; <?php echo date("Y"); ?> <?php echo $company->getBrandName(); ?>. All Rights Reserved.</span></li>
+            <li class="legal-list__item"><span class="legal-list__anchor">&copy; <?php echo date("Y"); ?> <?php echo $company->getBrandName(); ?>. All Rights Reserved.</span></li>
         </ul>
     </section>
 </footer>
