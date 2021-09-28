ae3966e47e48e2fdc1011ec24bcb5141df0fe242
wesleysmits <wesley@intermation.nl>
2021-09-28 20:32:14 +0200

    Cleanup old footer component styles

--- wp-content/themes/lavandre/assets/js/home.ts
+++ wp-content/themes/lavandre/assets/js/home.ts
@@ -1,11 +1,8 @@
-import '../css/pages/checkout.pcss';
-
 import Component from './common/Component';
 import Module from './Module';
 import StickyHeader from './components/StickyHeader';
 
 export default class HomeModule extends Module {
-
     public components: Component[] = [
         StickyHeader
     ];
