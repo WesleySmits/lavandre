f58894e7726d38a95f0184debcae4ed35a703883
wesleysmits <wesley@intermation.nl>
2021-10-31 18:10:31 +0100

    Cart panel styling fixes

--- wp-content/themes/lavandre/functions/woocommerce-tweaks.php
+++ wp-content/themes/lavandre/functions/woocommerce-tweaks.php
@@ -139,3 +139,17 @@
     function custom_add_to_cart_text() {
         return __('Add to Bag', 'lavandre');
     }
+
+    if ( ! function_exists( 'attribute_slug_to_title' ) ) {
+        function attribute_slug_to_title( $attribute ,$slug ) {
+            global $woocommerce;
+            if ( taxonomy_exists( esc_attr( str_replace( 'attribute_', '', $attribute ) ) ) ) {
+                $term = get_term_by( 'slug', $slug, esc_attr( str_replace( 'attribute_', '', $attribute ) ) );
+                if ( ! is_wp_error( $term ) && $term->name )
+                    $value = $term->name;
+            } else {
+                $value = apply_filters( 'woocommerce_variation_option_name', $value );
+            }
+            return $value;
+        }
+    }
