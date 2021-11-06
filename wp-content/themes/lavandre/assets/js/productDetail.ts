e8123111f76127fd5b7abf2fcffc29b34e17d8f5
wesleysmits <wesley@intermation.nl>
2021-11-06 15:41:56 +0100

    Added product detail view item enhanced ecommerce event

--- wp-content/themes/lavandre/assets/js/productDetail.ts
+++ wp-content/themes/lavandre/assets/js/productDetail.ts
@@ -5,6 +5,7 @@ import ChangeVariantColor from "./components/ChangeVariantColor";
 import ChangeVariantPrice from "./components/ChangeVariantPrice";
 import ImageGallery from "./components/ImageGallery";
 import Module from "./Module";
+import DataLayer from './common/DataLayer';
 
 export default class ProductDetail extends Module {
     public components: Component[] = [
@@ -14,6 +15,32 @@ export default class ProductDetail extends Module {
         ChangeVariantPrice,
         ChangeVariantColor
     ];
+
+    public initialize(): void {
+        super.initialize();
+
+        const data: GoogleAnalyticsProductDetailViewEvent = {
+            event: "view_item",
+            ecommerce: {
+                items: []
+            }
+        }
+
+        const form: HTMLFormElement | null = document.querySelector('form[data-product_id]');
+        const variationField: HTMLInputElement | null = document.querySelector('input[name="variation_id"]');
+
+        const productData: GoogleAnalyticsProduct = {
+            item_id: form?.dataset.product_id || '',
+            item_name: document.querySelector('.product-detail__title')?.textContent || '',
+            item_variant: variationField?.value || '',
+            price: Number(document.querySelector('[data-product-price]')?.textContent?.replace(',','.')) || 0,
+            item_brand: "Lavandré",
+        };
+
+        data.ecommerce.items.push(productData);
+
+        DataLayer.push(data);
+    }
 }
 
 (function productDetailInit() {
