51153f5e8205e93348d5db7e30972933300a02bb
Wesley Smits <wesley@intermation.nl>
2021-11-06 13:54:30 +0100

    [LV-X] Added GTM ID's to .env files (#68)

--- wp-content/themes/lavandre/header.php
+++ wp-content/themes/lavandre/header.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','<?php echo getGoogleTagManagerId(); ?>');</script>
+    })(window,document,'script','dataLayer','<?php echo $_ENV['GOOGLE_TAG_MANAGER_ID']; ?>');</script>
     <!-- End Google Tag Manager -->
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<?php $viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' ); ?>
@@ -31,7 +31,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <body <?php body_class(); ?>>
 
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo getGoogleTagManagerId(); ?>;"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $_ENV['GOOGLE_TAG_MANAGER_ID']; ?>;"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
