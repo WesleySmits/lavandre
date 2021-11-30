930e202c9adef15ea0384defdb4fd3a809690d56
Wesley Smits <wesley@intermation.nl>
2021-11-30 21:50:04 +0100

    Refactor search pages (#78)

--- wp-content/themes/lavandre/woocommerce/archive-product.php
+++ wp-content/themes/lavandre/woocommerce/archive-product.php
@@ -21,36 +21,36 @@ get_header( 'shop' );
 
 ?>
 
-<div class="full-width-banner-block">
-    <a class="full-width-banner-block__link" href="/product/organic-facial/">
-        <figure class="overlay-text">
-            <img class="cover-image" src="https://lavandre.com/wp-content/uploads/2021/09/mauro-lima-nfqDGE2WVGs-unsplash-scaled.jpeg" alt="">
-            <figcaption><?php _e('Shop face towels', 'lavandre'); ?></figcaption>
-        </figure>
-    </a>
-
-    <a class="full-width-banner-block__link" href="/product/organic-towel/">
-        <figure class="overlay-text">
-            <img class="cover-image" src="https://lavandre.com/wp-content/uploads/2021/09/pexels-agafonova-photo-5284980-scaled.jpeg" alt="">
-            <figcaption><?php _e('Shop hand / hair towels', 'lavandre'); ?></figcaption>
-        </figure>
-    </a>
-
-    <a class="full-width-banner-block__link" href="/product/organic-bath-towel/" id="bath-towel">
-        <figure class="overlay-text">
-            <img class="cover-image" src="https://lavandre.com/wp-content/uploads/2021/09/pexels-agafonova-photo-5276984-scaled.jpeg" alt="">
-            <figcaption><?php _e('Shop body towels', 'lavandre'); ?></figcaption>
-        </figure>
-    </a>
-
-    <div class="full-width-banner-block__see-more">
-        <a href="#shop-now" class="no-button" data-scroll-to="bath-towel" data-header-offset="false">
-            <?php include get_stylesheet_directory() . '/partials/icons/down-arrow.svg.php'; ?>
+<?php if (!is_search()) { ?>
+    <div class="full-width-banner-block">
+        <a class="full-width-banner-block__link" href="/product/organic-facial/">
+            <figure class="overlay-text">
+                <img class="cover-image" src="https://lavandre.com/wp-content/uploads/2021/09/mauro-lima-nfqDGE2WVGs-unsplash-scaled.jpeg" alt="">
+                <figcaption><?php _e('Shop face towels', 'lavandre'); ?></figcaption>
+            </figure>
         </a>
-    </div>
-</div>
 
-<?php
+        <a class="full-width-banner-block__link" href="/product/organic-towel/">
+            <figure class="overlay-text">
+                <img class="cover-image" src="https://lavandre.com/wp-content/uploads/2021/09/pexels-agafonova-photo-5284980-scaled.jpeg" alt="">
+                <figcaption><?php _e('Shop hand / hair towels', 'lavandre'); ?></figcaption>
+            </figure>
+        </a>
+
+        <a class="full-width-banner-block__link" href="/product/organic-bath-towel/" id="bath-towel">
+            <figure class="overlay-text">
+                <img class="cover-image" src="https://lavandre.com/wp-content/uploads/2021/09/pexels-agafonova-photo-5276984-scaled.jpeg" alt="">
+                <figcaption><?php _e('Shop body towels', 'lavandre'); ?></figcaption>
+            </figure>
+        </a>
+
+        <div class="full-width-banner-block__see-more">
+            <a href="#shop-now" class="no-button" data-scroll-to="bath-towel" data-header-offset="false">
+                <?php include get_stylesheet_directory() . '/partials/icons/down-arrow.svg.php'; ?>
+            </a>
+        </div>
+    </div>
+<?php } else {
     /**
      * Hook: woocommerce_before_main_content.
      *
@@ -58,43 +58,64 @@ get_header( 'shop' );
      * @hooked woocommerce_breadcrumb - 20
      * @hooked WC_Structured_Data::generate_website_data() - 30
      */
-    // do_action( 'woocommerce_before_main_content' );
+    do_action( 'woocommerce_before_main_content' );
 
-    // if ( woocommerce_product_loop() ) {
-    //     woocommerce_product_loop_start();
+    $title = sprintf(__('Search results for "%s".', 'lavandre'), get_search_query());
 
-    //     if ( wc_get_loop_prop( 'total' ) ) {
-    //         while ( have_posts() ) {
-    //             the_post();
+    if (!wc_get_loop_prop('total')) {
+        $title = sprintf(__('Sorry, nothing found for "%s".', 'lavandre'), get_search_query());
+    }
+        ?>
+            <section class="product-overview__search ww-container">
+                <header>
+                    <p><?php echo $title; ?></p>
+                </header>
 
-    //             ? >
+                <?php include get_stylesheet_directory() . '/woocommerce/product-searchform.php'; ?>
+        <?php
 
-    //             <li class="ww-products__item">
-    //                 <a class="ww-products__link" href="<?php the_permalink(); ? >">
-    //                     <?php $image = wp_get_attachment_image_src(218, [438,584]); ? >
+            if ( woocommerce_product_loop() ) {
+                woocommerce_product_loop_start();
 
-    //                     <!-- <span class="badge onsale perc">15%</span> -->
+                if ( wc_get_loop_prop( 'total' ) ) {
+                    while ( have_posts() ) {
+                        the_post();
 
-    //                     <img
-    //                         src="<?php echo get_the_post_thumbnail_url(get_the_ID(), array( 438, 584) ); ? >"
-    //                         srcset="<?php echo get_the_post_thumbnail_url(get_the_ID(), array( 438, 584) ); ? > 1x, <?php echo get_the_post_thumbnail_url(get_the_ID(), array( 876, 1168) ); ? > 2x, <?php echo get_the_post_thumbnail_url(get_the_ID(), array( 1314, 1752) ); ? > 3x"
-    //                         class="ww-products__image"
-    //                         alt="<?php the_title(); ? >"
-    //                         width="438"
-    //                         height="584"
-    //                         loading="lazy"
-    //                     >
+                        ?>
 
-    //                     <h2 class="ww-products__title">
-    //                         <?php the_title(); ? >
-    //                     </h2>
-    //                 </a>
-    //             </li>
+                        <li class="ww-products__item">
+                            <a class="ww-products__link" href="<?php the_permalink(); ?>">
+                                <?php $image = wp_get_attachment_image_src(218, [438,584]); ?>
 
-    //             <?php
-    //         }
-    //     }
-    //     woocommerce_product_loop_end();
-    // }
+                                <!-- <span class="badge onsale perc">15%</span> -->
 
-    get_footer();
+                                <img
+                                    src="<?php echo get_the_post_thumbnail_url(get_the_ID(), array( 438, 584) ); ?>"
+                                    srcset="<?php echo get_the_post_thumbnail_url(get_the_ID(), array( 438, 584) ); ?> 1x, <?php echo get_the_post_thumbnail_url(get_the_ID(), array( 876, 1168) ); ?> 2x, <?php echo get_the_post_thumbnail_url(get_the_ID(), array( 1314, 1752) ); ?> 3x"
+                                    class="ww-products__image"
+                                    alt="<?php the_title(); ?>"
+                                    width="438"
+                                    height="584"
+                                    loading="lazy"
+                                >
+
+                                <h2 class="ww-products__title">
+                                    <?php the_title(); ?>
+                                </h2>
+
+                                <button is="lavandre-button" outline size="large" full-width href="<?php the_permalink(); ?>">
+                                    <?php echo _e('Add to Bag'); ?>
+                                </button>
+                            </a>
+                        </li>
+
+                        <?php
+                    }
+                }
+                woocommerce_product_loop_end();
+            }
+        ?></section><?php
+    do_action( 'woocommerce_after_main_content' );
+}
+
+get_footer();
