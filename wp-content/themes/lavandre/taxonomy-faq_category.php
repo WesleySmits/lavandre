440a47b578267cb6c9719858fccc079d57a8f64f
wesleysmits <wesley@intermation.nl>
2021-12-04 16:05:47 +0100

    Accordion clean-up

--- wp-content/themes/lavandre/taxonomy-faq_category.php
+++ wp-content/themes/lavandre/taxonomy-faq_category.php
@@ -36,32 +36,27 @@
         </nav>
 
         <div class="page-content" itemscope itemtype="https://schema.org/FAQPage">
-            <?php
-                if ( have_posts() ) {
-                    ?><ul class="accordion-list"><?php
-                    while ( have_posts() ) {
-                        the_post();
+            <?php if ( have_posts() ) { ?>
+                <accordion-element class="legal" data-cy="page-accordion">
+                    <?php while ( have_posts() ) { ?>
+                        <?php the_post(); ?>
+                        <details is="curtain-element" itemprop="mainEntity" itemscope itemtype="http://schema.org/Question">
+                            <summary>
+                                <a href="<?php echo get_the_permalink();?>" itemprop="name"><?php echo get_the_title(); ?></a>
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="15px" height="15px" viewBox="0 0 15 15" xml:space="preserve" aria-hidden="true" data-acsb-hidden="true" data-acsb-force-hidden="true">
+                                    <g fill="#2b2b2b">
+                                        <polygon points="0.104,4.333 1.165,3.272 7.5,9.607 13.835,3.272 14.896,4.333 7.5,11.728 "></polygon>
+                                    </g>
+                                </svg>
+                            </summary>
 
-                        ?>
-                        <li>
-                            <article itemprop="mainEntity" itemscope itemtype="http://schema.org/Question">
-                                <header>
-                                    <h3 itemprop="name">
-                                        <a href="<?php echo get_the_permalink();?>" data-curtain-toggle="<?php echo 'accordion-' . get_the_ID()?>">
-                                            <?php echo get_the_title(); ?>
-                                        </a>
-                                    </h3>
-                                </header>
-
-                                <div data-curtain-content="<?php echo 'accordion-' . get_the_ID()?>" itemprop="acceptedAnswer" itemscope itemtype="https://schema.org/Answer">
-                                    <div itemprop="text"><?php echo get_the_content(); ?></div>
-                                </div>
-                            </article>
-                        </li>
-                        <?php
-                    }
-                    ?></ul><?php
-                }?>
+                            <div class="curtain-content" itemprop="acceptedAnswer" itemscope itemtype="https://schema.org/Answer">
+                                <div itemprop="text"><?php echo get_the_content(); ?></div>
+                            </div>
+                        </details>
+                    <?php } ?>
+                </accordion-element>
+            <?php } ?>
         </div>
     </div>
 </main>
