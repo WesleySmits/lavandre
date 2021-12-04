e207f0cee8be8d6b7bd19911932ae5c1e189ee41
Wesley Smits <wesley@intermation.nl>
2021-12-04 16:14:17 +0100

    Accordion cleanup (#79)
    * Accordion clean-up
    
    * Fix
    
    * Fix

--- wp-content/themes/lavandre/template-parts/contact.php
+++ wp-content/themes/lavandre/template-parts/contact.php
@@ -16,35 +16,26 @@ get_header();
         </header>
     <?php endif; ?>
     <div class="page-content">
-        <?php
-            if( have_rows('accordion') ):
-                $index = 0;
-                ?><ul class="accordion-list accordion-list--legal"><?php
-                while( have_rows('accordion') ) : the_row();
-                    $title = get_sub_field('title');
-                    $description = get_sub_field('description');
-                    $id = "page-accordion-" . $index;
+        <?php if( have_rows('accordion') ): ?>
+            <accordion-element class="legal" data-cy="page-accordion">
+                <?php while( have_rows('accordion') ) : the_row(); ?>
+                    <details is="curtain-element">
+                        <summary>
+                            <span><?php echo get_sub_field('title') ?></span>
+                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="15px" height="15px" viewBox="0 0 15 15" xml:space="preserve" aria-hidden="true" data-acsb-hidden="true" data-acsb-force-hidden="true">
+                                <g fill="#2b2b2b">
+                                    <polygon points="0.104,4.333 1.165,3.272 7.5,9.607 13.835,3.272 14.896,4.333 7.5,11.728 "></polygon>
+                                </g>
+                            </svg>
+                        </summary>
 
-                    ?>
-                        <li>
-                            <article>
-                                <header>
-                                    <h3 data-curtain-toggle="<?php echo $id; ?>"><?php echo $title; ?></h3>
-                                </header>
-
-                                <div data-curtain-content="<?php echo $id; ?>">
-                                    <?php echo $description; ?>
-                                </div>
-                            </article>
-                        </li>
-                    <?php
-
-                    $index++;
-                endwhile;
-
-                ?></ul><?php
-            endif;
-        ?>
+                        <div class="curtain-content">
+                            <?php echo get_sub_field('description'); ?>
+                        </div>
+                    </details>
+                <?php endwhile; ?>
+            </accordion-element>
+        <?php endif; ?>
 
         <?php the_content(); ?>
         <div class="post-tags">
