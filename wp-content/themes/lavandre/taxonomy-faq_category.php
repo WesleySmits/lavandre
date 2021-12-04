53bd5040ff5536604eedef0301f15bb1febd529b
wesleysmits <wesley@intermation.nl>
2021-12-04 22:07:33 +0100

    Refactor customer service pages

--- wp-content/themes/lavandre/taxonomy-faq_category.php
+++ wp-content/themes/lavandre/taxonomy-faq_category.php
@@ -10,10 +10,19 @@
         exit;
     }
     get_header();
+
+    $term = get_queried_object();
+    $top_banner = get_field('top_banner', $term);
+    $top_banner_title = get_field('top_banner_title', $term);
 ?>
 
 <main <?php post_class( 'site-main' ); ?> role="main">
     <div class="page-wrapper">
+        <header class="ww-page-banner">
+            <img src="<?php echo $top_banner ?>" alt="" />
+            <h1 class="ww-page-banner__overlay ww-page-banner__title ww-page-banner__title--large"><?php echo $top_banner_title; ?></h1>
+        </header>
+
         <nav class="side-navigation">
            <ul>
             <?php
@@ -37,25 +46,16 @@
 
         <div class="page-content" itemscope itemtype="https://schema.org/FAQPage">
             <?php if ( have_posts() ) { ?>
-                <accordion-element class="legal" data-cy="page-accordion">
-                    <?php while ( have_posts() ) { ?>
-                        <?php the_post(); ?>
-                        <details is="curtain-element" itemprop="mainEntity" itemscope itemtype="http://schema.org/Question">
-                            <summary>
-                                <a href="<?php echo get_the_permalink();?>" itemprop="name"><?php echo get_the_title(); ?></a>
-                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="15px" height="15px" viewBox="0 0 15 15" xml:space="preserve" aria-hidden="true" data-acsb-hidden="true" data-acsb-force-hidden="true">
-                                    <g fill="#2b2b2b">
-                                        <polygon points="0.104,4.333 1.165,3.272 7.5,9.607 13.835,3.272 14.896,4.333 7.5,11.728 "></polygon>
-                                    </g>
-                                </svg>
-                            </summary>
+                <?php while ( have_posts() ) { ?>
+                    <?php the_post(); ?>
+                    <div class="question">
+                        <h2 itemprop="name"><?php echo get_the_title(); ?></h2>
 
-                            <div class="curtain-content" itemprop="acceptedAnswer" itemscope itemtype="https://schema.org/Answer">
-                                <div itemprop="text"><?php echo get_the_content(); ?></div>
-                            </div>
-                        </details>
-                    <?php } ?>
-                </accordion-element>
+                        <div itemprop="acceptedAnswer" itemscope itemtype="https://schema.org/Answer">
+                            <div itemprop="text"><?php echo get_the_content(); ?></div>
+                        </div>
+                    </div>
+                <?php } ?>
             <?php } ?>
         </div>
     </div>
