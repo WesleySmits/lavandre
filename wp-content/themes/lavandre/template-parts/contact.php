8dcb69b4849f5d206ff0102f84ca6ed932153aa9
wesleysmits <wesley@intermation.nl>
2021-12-12 20:43:16 +0100

    Clean up contact.php

--- wp-content/themes/lavandre/template-parts/contact.php
+++ wp-content/themes/lavandre/template-parts/contact.php
@@ -16,27 +16,6 @@ get_header();
         </header>
     <?php endif; ?>
     <div class="page-content">
-        <?php if( have_rows('accordion') ): ?>
-            <accordion-element class="legal" data-cy="page-accordion">
-                <?php while( have_rows('accordion') ) : the_row(); ?>
-                    <details is="curtain-element">
-                        <summary>
-                            <span><?php echo get_sub_field('title') ?></span>
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="15px" height="15px" viewBox="0 0 15 15" xml:space="preserve" aria-hidden="true" data-acsb-hidden="true" data-acsb-force-hidden="true">
-                                <g fill="#2b2b2b">
-                                    <polygon points="0.104,4.333 1.165,3.272 7.5,9.607 13.835,3.272 14.896,4.333 7.5,11.728 "></polygon>
-                                </g>
-                            </svg>
-                        </summary>
-
-                        <div class="curtain-content">
-                            <?php echo get_sub_field('description'); ?>
-                        </div>
-                    </details>
-                <?php endwhile; ?>
-            </accordion-element>
-        <?php endif; ?>
-
         <?php the_content(); ?>
         <div class="post-tags">
             <?php the_tags( '<span class="tag-links">' . __( 'Tagged ', 'hello-elementor' ), null, '</span>' ); ?>
