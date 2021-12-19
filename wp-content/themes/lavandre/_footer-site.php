69d5bb7c39ce0a6be3ae9cf72c42a5a80bc2f7fa
Wesley Smits <wesley@intermation.nl>
2021-12-19 21:19:06 +0100

    Footer link changes (#95)
    * Change footer links
    
    * Fix in deploy script

--- wp-content/themes/lavandre/_footer-site.php
+++ wp-content/themes/lavandre/_footer-site.php
@@ -30,20 +30,41 @@
                 <div class="ww-footer__subscribe-form-wrapper">
                     <form class="ww-form" id="footer-newsletter-form" action="post">
                         <p class="ww-form__field">
-                            <input type="email" name="email" id="footer-newsletter-form-email" title="<?php _e('Please enter a valid e-mail address', 'lavandre'); ?>" placeholder="<?php _e('Email', 'lavandre'); ?>">
+                            <input type="email" name="email" id="footer-newsletter-form-email"
+                                title="<?php _e('Please enter a valid e-mail address', 'lavandre'); ?>"
+                                placeholder="<?php _e('Email', 'lavandre'); ?>">
                         </p>
-                        <button is="lavandre-button" outline size="large" type="submit" id="footer-newsletter-form-submit"><?php _e('Submit', 'lavandre'); ?></button>
+                        <button is="lavandre-button" outline size="large" type="submit"
+                            id="footer-newsletter-form-submit"><?php _e('Submit', 'lavandre'); ?></button>
                     </form>
                 </div>
 
                 <section class="ww-footer__social-links">
                     <ul class="socialmedia-list no-list">
-                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor" href="mailto:<?php echo $contactPoint->getEmailAddress(); ?>" aria-label="E-mail"><?php include get_stylesheet_directory() . '/partials/icons/envelope.svg.php'; ?></a></li>
-                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor" target="_blank" rel="noopener" href="<?php echo $contactPoint->getFacebook(); ?>" aria-label="Facebook"><?php include get_stylesheet_directory() . '/partials/icons/facebook-f.svg.php'; ?></a></li>
-                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor" target="_blank" rel="noopener" href="<?php echo $contactPoint->getInstagram(); ?>" aria-label="Instagram"><?php include get_stylesheet_directory() . '/partials/icons/instagram.svg.php'; ?></a></li>
-                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor" target="_blank" rel="noopener" href="<?php echo $contactPoint->getYoutube(); ?>" aria-label="YouTube"><?php include get_stylesheet_directory() . '/partials/icons/youtube.svg.php'; ?></a></li>
-                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor" target="_blank" rel="noopener" href="<?php echo $contactPoint->getTwitter(); ?>" aria-label="Twitter"><?php include get_stylesheet_directory() . '/partials/icons/twitter.svg.php'; ?></a></li>
-                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor" target="_blank" rel="noopener" href="<?php echo $contactPoint->getLinkedin(); ?>" aria-label="LinkedIn"><?php include get_stylesheet_directory() . '/partials/icons/linkedin.svg.php'; ?></a></li>
+                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor"
+                                href="mailto:<?php echo $contactPoint->getEmailAddress(); ?>"
+                                aria-label="E-mail"><?php include get_stylesheet_directory() . '/partials/icons/envelope.svg.php'; ?></a>
+                        </li>
+                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor" target="_blank"
+                                rel="noopener" href="<?php echo $contactPoint->getFacebook(); ?>"
+                                aria-label="Facebook"><?php include get_stylesheet_directory() . '/partials/icons/facebook-f.svg.php'; ?></a>
+                        </li>
+                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor" target="_blank"
+                                rel="noopener" href="<?php echo $contactPoint->getInstagram(); ?>"
+                                aria-label="Instagram"><?php include get_stylesheet_directory() . '/partials/icons/instagram.svg.php'; ?></a>
+                        </li>
+                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor" target="_blank"
+                                rel="noopener" href="<?php echo $contactPoint->getYoutube(); ?>"
+                                aria-label="YouTube"><?php include get_stylesheet_directory() . '/partials/icons/youtube.svg.php'; ?></a>
+                        </li>
+                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor" target="_blank"
+                                rel="noopener" href="<?php echo $contactPoint->getTwitter(); ?>"
+                                aria-label="Twitter"><?php include get_stylesheet_directory() . '/partials/icons/twitter.svg.php'; ?></a>
+                        </li>
+                        <li class="socialmedia-list__item"><a class="socialmedia-list__anchor" target="_blank"
+                                rel="noopener" href="<?php echo $contactPoint->getLinkedin(); ?>"
+                                aria-label="LinkedIn"><?php include get_stylesheet_directory() . '/partials/icons/linkedin.svg.php'; ?></a>
+                        </li>
                     </ul>
                 </section>
             </section>
@@ -52,43 +73,56 @@
         <div class="ww-footer__column ww-footer__link">
             <section class="ww-footer__links__column">
                 <header class="ww-footer__links__header">
-                    <h3 class="footer__h3" data-curtain-toggle="footer-column-1"><?php _e('Customer service', 'lavandre'); ?></h3>
+                    <h3 class="footer__h3" data-curtain-toggle="footer-column-1">
+                        <?php _e('Customer service', 'lavandre'); ?></h3>
                 </header>
 
                 <ul class="no-list" data-curtain-content="footer-column-1" data-only-on-mobile="true">
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/contact/"><?php _e('Contact', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/customer-service/payment/"><?php _e('Payment', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/customer-service/orders/"><?php _e('Orders', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/customer-service/shipping/"><?php _e('Shipping', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/customer-service/returns/"><?php _e('Returns', 'lavandre'); ?></a></li>
+                    <li class="footer-links__item"><a class="footer-links__anchor"
+                            href="/contact/"><?php _e('Contact', 'lavandre'); ?></a></li>
+                    <li class="footer-links__item"><a class="footer-links__anchor"
+                            href="/customer-service/payment/"><?php _e('Payment', 'lavandre'); ?></a></li>
+                    <li class="footer-links__item"><a class="footer-links__anchor"
+                            href="/customer-service/orders/"><?php _e('Orders', 'lavandre'); ?></a></li>
+                    <li class="footer-links__item"><a class="footer-links__anchor"
+                            href="/customer-service/shipping/"><?php _e('Shipping', 'lavandre'); ?></a></li>
+                    <li class="footer-links__item"><a class="footer-links__anchor"
+                            href="/customer-service/returns/"><?php _e('Returns', 'lavandre'); ?></a></li>
                 </ul>
             </section>
 
             <section class="ww-footer__links__column">
                 <header class="ww-footer__links__header">
-                    <h3 class="footer__h3" data-curtain-toggle="footer-column-2"><?php _e('Online orders', 'lavandre'); ?></h3>
+                    <h3 class="footer__h3" data-curtain-toggle="footer-column-2">
+                        <?php _e('About Lavandré', 'lavandre'); ?></h3>
                 </header>
 
                 <ul class="no-list" data-curtain-content="footer-column-2" data-only-on-mobile="true">
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/my-account/"><?php _e('My account', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/customer-service/payment/"><?php _e('Payment', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/customer-service/orders/"><?php _e('Orders', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/customer-service/shipping/"><?php _e('Shipping', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/customer-service/returns/"><?php _e('Returns', 'lavandre'); ?></a></li>
+                    <li class="footer-links__item">
+                        <a class="footer-links__anchor" href="/about/"><?php _e('About us', 'lavandre'); ?></a>
+                    </li>
+
+                    <li class="footer-links__item">
+                        <a class="footer-links__anchor" href="/blog/"><?php _e('Blog', 'lavandre'); ?></a>
+                    </li>
+
+                    <li class="footer-links__item">
+                        <a class="footer-links__anchor"
+                            href="/sustainability/"><?php _e('Sustainability', 'lavandre'); ?></a>
+                    </li>
                 </ul>
             </section>
 
             <section class="ww-footer__links__column">
                 <header class="ww-footer__links__header">
-                    <h3 class="footer__h3" data-curtain-toggle="footer-column-3"><?php _e('Sustainability', 'lavandre'); ?></h3>
+                    <h3 class="footer__h3" data-curtain-toggle="footer-column-3">
+                        <?php _e('Information', 'lavandre'); ?></h3>
                 </header>
 
                 <ul class="no-list" data-curtain-content="footer-column-3" data-only-on-mobile="true">
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/sustainability/"><?php _e('Sustainability', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/sustainability/acting-responsibly/"><?php _e('Acting responsibly', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/sustainability/sustainable-production/"><?php _e('Sustainable production', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/sustainability/derived-from-nature/"><?php _e('Derived from nature', 'lavandre'); ?></a></li>
-                    <li class="footer-links__item"><a class="footer-links__anchor" href="/sustainability/contributing-to-nature/"><?php _e('Contributing to nature', 'lavandre'); ?></a></li>
+                    <li class="footer-links__item">
+                        <a class="footer-links__anchor" href="/my-account/"><?php _e('My account', 'lavandre'); ?></a>
+                    </li>
                 </ul>
             </section>
         </div>
@@ -96,9 +130,12 @@
 
     <section class="ww-footer__bottom">
         <ul class="legal-list no-list inline-list">
-            <li class="legal-list__item"><a class="legal-list__anchor" href="/terms-and-conditions/"><?php _e('Terms of use', 'lavandre'); ?></a></li>
-            <li class="legal-list__item"><a class="legal-list__anchor" href="/privacy-policy/"><?php _e('Privacy policy', 'lavandre'); ?></a></li>
-            <li class="legal-list__item"><span class="legal-list__anchor">&copy; <?php echo date("Y"); ?> <?php echo $company->getBrandName(); ?>. All Rights Reserved.</span></li>
+            <li class="legal-list__item"><a class="legal-list__anchor"
+                    href="/terms-and-conditions/"><?php _e('Terms of use', 'lavandre'); ?></a></li>
+            <li class="legal-list__item"><a class="legal-list__anchor"
+                    href="/privacy-policy/"><?php _e('Privacy policy', 'lavandre'); ?></a></li>
+            <li class="legal-list__item"><span class="legal-list__anchor">&copy; <?php echo date("Y"); ?>
+                    <?php echo $company->getBrandName(); ?>. All Rights Reserved.</span></li>
         </ul>
     </section>
 </footer>
