53b300c51d65528f68ef963ffb1a381a2743eb02
wesleysmits <wesley@intermation.nl>
2021-12-21 18:26:45 +0100

    Refactor cookie bar

--- wp-content/themes/lavandre/assets/js/components/CookieBar.ts
+++ wp-content/themes/lavandre/assets/js/components/CookieBar.ts
@@ -3,69 +3,53 @@ import Component from '../common/Component';
 import { getCookieValue, setCookieValue } from '../util/cookies';
 
 export default class CookieBar extends Component {
-    private element: HTMLElement | null = null;
+    #element: HTMLElement | null = null;
 
-    private templateElement: HTMLTemplateElement;
+    #templateElement: HTMLTemplateElement;
 
-    private acceptButton: HTMLButtonElement | null = null;
-
-    private rejectButton: HTMLButtonElement | null = null;
+    #acceptButton: HTMLButtonElement | null = null;
 
     static cookieName: string = 'approved_cookies';
 
     constructor(selector: Document | HTMLElement = document) {
         super();
-        this.templateElement = selector.querySelector(
+        this.#templateElement = selector.querySelector(
             '#cookie-bar-template'
         )! as HTMLTemplateElement;
     }
 
     public initialize(): void {
-        if (this.templateElement === null) {
+        if (this.#templateElement === null) {
             return;
         }
 
         if (getCookieValue(CookieBar.cookieName)) {
-            this.templateElement.remove();
+            this.#templateElement.remove();
             return;
         }
 
-        this.element = this.getElement();
+        this.#element = this.#getElement();
 
-        this.acceptButton = this.element.querySelector('[data-role="cookies-accept"]');
-        this.rejectButton = this.element.querySelector('[data-role="cookies-reject"]');
-
-        this.acceptButton?.addEventListener('click', this.acceptCookies.bind(this));
-        this.rejectButton?.addEventListener('click', this.rejectCookies.bind(this));
+        this.#acceptButton = this.#element.querySelector('[data-role="cookies-accept"]');
+        this.#acceptButton?.addEventListener('click', this.#acceptCookies.bind(this));
     }
 
-    private getElement(): HTMLElement {
-        const clone: HTMLTemplateElement = this.templateElement.cloneNode(
+    #getElement(): HTMLElement {
+        const clone: HTMLTemplateElement = this.#templateElement.cloneNode(
             true
         ) as HTMLTemplateElement;
         const { content } = clone;
-        this.templateElement.parentElement!.appendChild(content);
+        this.#templateElement.parentElement!.appendChild(content);
         return document.querySelector('[data-role="cookie-bar"]') as HTMLElement;
     }
 
-    private acceptCookies(): void {
+    #acceptCookies(): void {
         CookieBar.setAcceptCookie();
-        this.element?.remove();
-    }
-
-    private rejectCookies(): void {
-        CookieBar.setRejectCookie();
-        this.element?.remove();
+        this.#element?.remove();
     }
 
     static setAcceptCookie(): void {
         setCookieValue(CookieBar.cookieName, 'true', {
-            'max-age': 31536000
-        });
-    }
-
-    static setRejectCookie(): void {
-        setCookieValue(CookieBar.cookieName, 'false', {
             'max-age': 31536000
         });
     }
