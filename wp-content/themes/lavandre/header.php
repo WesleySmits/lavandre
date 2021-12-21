9e98a098b6bd0733997327bb70f7ad9cd55f90a1
Wesley Smits <wesley@intermation.nl>
2021-12-21 19:24:04 +0100

    Back env prod (#98)
    * Bring back .env.production
    
    * Fix

--- wp-content/themes/lavandre/header.php
+++ wp-content/themes/lavandre/header.php
@@ -13,29 +13,42 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','<?php echo $_ENV['GOOGLE_TAG_MANAGER_ID']; ?>');</script>
+    <script>
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', '<?php echo $_ENV['GOOGLE_TAG_MANAGER_ID'] ?? 'GTM-WRKTQ4W'; ?>');
+    </script>
     <!-- End Google Tag Manager -->
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<?php $viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' ); ?>
-	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <?php $viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' ); ?>
+    <meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/wp-content/themes/lavandre/partials/_head.php'; ?>
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $_ENV['GOOGLE_TAG_MANAGER_ID']; ?>;"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $_ENV['GOOGLE_TAG_MANAGER_ID']; ?>;"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
-<?php
+    <?php
     if (is_wc_endpoint_url('order-received')) {
         return;
     } else if (is_checkout()) {
