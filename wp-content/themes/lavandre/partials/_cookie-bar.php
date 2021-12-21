21a6571888bbd81fd7a0f05b6a8c356398181cb7
Wesley Smits <wesley@intermation.nl>
2021-12-21 18:48:36 +0100

    Cookie bar refactor + Newsletter popup removal (#96)
    * Refactor cookie bar
    
    * Remove newsletter popup
    
    * Remove newsletter popup

--- wp-content/themes/lavandre/partials/_cookie-bar.php
+++ wp-content/themes/lavandre/partials/_cookie-bar.php
@@ -1,12 +1,12 @@
-<template id="cookie-bar-template" data-test="test">
-    <aside class="cookie-bar ww-container" data-role="cookie-bar" data-test="test2">
+<template id="cookie-bar-template">
+    <aside class="cookie-bar ww-container" data-role="cookie-bar">
         <p>
-            <?php _e('Lavandré uses cookies and comparable techniques such as link tracking to make the visit and shopping at the Lavandre website even easier and more personal for you. With these cookies we and third parties can track your internet behavior within and possibly also outside our website and when you click through from our e-mails. This allows us and third parties to adapt advertisements and other content to your interests and you can share information via social media. If you do not agree, we only place cookies to make the website work and to perform analyzes.', 'lavandre'); ?>
+            <?php _e('This website uses cookies to ensure you get the best experience.', 'lavandre'); ?>
+            <a href="/privacy-policy/"><?php _e('Learn more', 'lavandre'); ?></a>
         </p>
 
-        <div class="flex">
-            <button is="lavandre-button" size="large" full-width outline type="button" data-role="cookies-reject"><?php _e('Reject', 'lavandre'); ?></button>
-            <button is="lavandre-button" size="large" full-width primary type="button" data-role="cookies-accept"><?php _e('Agree', 'lavandre'); ?></button>
-        </div>
+        <button class="cookie-bar__close no-button" type="button" data-role="cookies-accept">
+            <?php _e('X', 'lavandre'); ?>
+        </button>
     </aside>
 </template>
