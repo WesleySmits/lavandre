0400ebdf376f6aa0fca9e31d0c2f73a12871008f
Wesley Smits <wesley@intermation.nl>
2021-12-27 20:55:59 +0100

    Lazyload fixes (#112)

--- wp-content/themes/lavandre/woocommerce/single-product/product-thumbnails.php
+++ wp-content/themes/lavandre/woocommerce/single-product/product-thumbnails.php
@@ -28,31 +28,27 @@ $attachment_ids = $product->get_gallery_image_ids();
 
 if ( $attachment_ids && $product->get_image_id() ) {
 	?>
-		<ul class="product-detail__gallery__thumbnails">
-            <?php
+<ul class="product-detail__gallery__thumbnails">
+    <?php
                 foreach ( $attachment_ids as $key => $attachment_id ) { ?>
-                    <?php
+    <?php
                         $width = 118;
                         $height = 158;
                         $image = wp_get_attachment_image_src($attachment_id, [$width,$height]);
                         $image_alt = get_post_meta($attachment_id, '_wp_attachment_image_alt', TRUE);
                     ?>
-                    <li data-lazyload>
-                        <a href="<?php echo get_image_kit_url($image[0]); ?>" target="_blank" data-thumbnail-id="<?php echo $attachment_id; ?>"">
+    <li data-lazyload>
+        <a href="<?php echo get_image_kit_url($image[0]); ?>" target="_blank"
+            data-thumbnail-id="<?php echo $attachment_id; ?>"">
                             <img
-                                src="<?php echo get_image_kit_placeholder($image[0], 1, 1) ?>"
-                                data-src="<?php echo get_image_kit_url($image[0]); ?>"
-                                class="ww-products__image loading"
-                                alt="<?php echo $image_alt; ?>"
-                                width="<?php echo $width; ?>"
-                                height="<?php echo $height ?>"
-                                loading="lazy"
-                            >
-                        </a>
-                    </li>
-                    <?php
+                                src=" <?php echo get_image_kit_placeholder($image[0], 1, 1) ?>"
+            data-src="<?php echo get_image_kit_url($image[0]); ?>" class="ww-products__image loading"
+            alt="<?php echo $image_alt; ?>" width="<?php echo $width; ?>" height="<?php echo $height ?>" loading="lazy">
+        </a>
+    </li>
+    <?php
                 }
             ?>
-		</ul>
-	<?php
+</ul>
+<?php
 }
