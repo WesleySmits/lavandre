28f9c7c8bddccf382909c1a6e22283b977700514
wesleysmits <wesley@intermation.nl>
2022-01-01 20:48:02 +0100

    Mobile menu changes

--- wp-content/themes/lavandre/cypress/integration/en/components/mobile-menu.spec.ts
+++ wp-content/themes/lavandre/cypress/integration/en/components/mobile-menu.spec.ts
@@ -4,14 +4,11 @@ const mobileMenuToggleSelector: string = '[data-panel="mobile-menu"]';
 const mobileMenuSelector: string = '[data-panel-name="mobile-menu"]';
 
 const navigationPages: string[] = [
-    'Homepage',
     'Shop',
-    'My account',
-    'Request sample',
     'Sustainability',
+    'Blog',
     'Contact',
-    'Log in',
-    'Register'
+    'My account',
 ];
 
 describe('Mobile menu tests - mobile', () => {
@@ -25,12 +22,6 @@ describe('Mobile menu tests - mobile', () => {
     it('should close the mobile menu', () => {
         cy.get(mobileMenuSelector).find('.side-panel__close').click();
         cy.get(mobileMenuSelector).first().should('not.be.visible');
-    });
-
-    it('should perform a search function', () => {
-        cy.get(mobileMenuSelector).find('input[type="search"]').first().type('test');
-        cy.get(mobileMenuSelector).find('form[role="search"]').submit();
-        cy.url().should('contain', 's=test');
     });
 
     navigationPages.forEach((page: string) => {
--- wp-content/themes/lavandre/partials/panels/_mobile-menu.php
+++ wp-content/themes/lavandre/partials/panels/_mobile-menu.php
@@ -1,25 +1,14 @@
 <template data-panel-template="mobile-menu">
-    <dialog class="side-panel side-panel--left" data-panel-name="mobile-menu">
+    <dialog class="side-panel side-panel--left side-panel--mobile-menu" data-panel-name="mobile-menu">
         <div class="side-panel__container">
-            <header class="side-panel__header">
-                <img src="/wp-content/themes/lavandre/public/images/logos/lavandre-logo-open.svg" alt="Lavandré" width="150" height="26" loading="lazy">
-
+            <header class="side-panel__header side-panel__header--no-border">
                 <button type="button" data-close class="side-panel__close">
                     <?php include get_stylesheet_directory() . '/partials/icons/close.svg.php'; ?>
                 </button>
             </header>
 
-            <section class="side-panel__section side-panel__section--alt">
-                <?php include get_stylesheet_directory() . '/woocommerce/product-searchform.php'; ?>
-            </section>
-
             <nav class="side-panel__section side-panel__navigation">
                 <ul class="no-list">
-                    <li>
-                        <a href="/">
-                            <span><?php _e('Homepage', 'lavandre'); ?></span>
-                        </a>
-                    </li>
 
                     <li>
                         <a href="/shop">
@@ -28,57 +17,21 @@
                     </li>
 
                     <li>
-                        <a href="#">
-                            <span><?php _e('Branches', 'lavandre'); ?></span>
+                        <a href="/sustainability">
+                            <span><?php _e('Sustainability', 'lavandre'); ?></span>
                         </a>
                     </li>
+
+                    <li>
+                        <a href="/blog">
+                            <span><?php _e('Blog', 'lavandre'); ?></span>
+                        </a>
+                    </li>
+
 
                     <li>
                         <a href="/my-account">
                             <span><?php _e('My account', 'lavandre'); ?></span>
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="/product/sample-collection/">
-                            <span><?php _e('Request sample', 'lavandre'); ?></span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-
-            <section class="side-panel__section side-panel__section--alt"></section>
-
-            <nav class="side-panel__section side-panel__usps">
-                <ul class="no-list flex">
-                    <li>
-                        <a href="/my-account">
-                            <?php include $_SERVER['DOCUMENT_ROOT'] . '/wp-content/themes/lavandre/partials/icons/user.svg.php'; ?>
-                            <span><?php _e('Log in', 'lavandre'); ?></span>
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="/my-account">
-                            <?php include $_SERVER['DOCUMENT_ROOT'] . '/wp-content/themes/lavandre/partials/icons/user-register.svg.php'; ?>
-                            <span><?php _e('Register', 'lavandre'); ?></span>
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="/product/sample-collection/">
-                            <?php include $_SERVER['DOCUMENT_ROOT'] . '/wp-content/themes/lavandre/partials/icons/gift.svg.php'; ?>
-                            <span><?php _e('Request sample', 'lavandre'); ?></span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-
-            <nav class="side-panel__section side-panel__section--alt side-panel__navigation">
-                <ul class="no-list">
-                    <li>
-                        <a href="/sustainability">
-                            <span><?php _e('Sustainability', 'lavandre'); ?></span>
                         </a>
                     </li>
 
