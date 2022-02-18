9e0fc715aa4740407923b9f1fe0d6cba39431a7f
Wesley Smits <wesley@intermation.nl>
2022-02-18 21:48:14 +0100

    Fix issue with my account rewards page

--- wp-content/themes/lavandre/functions/accountHandler.php
+++ wp-content/themes/lavandre/functions/accountHandler.php
@@ -71,5 +71,21 @@ Class LavandreAccountPages
 
     function moveElement(&$array, $fromIndex, $toIndex) {
         $out = array_splice($array, $fromIndex, 1);
-        array_splice($array, $toIndex, 0, $out);
+        array_splice_assoc($array, $toIndex, 0, $out);
     }
+
+    function array_splice_assoc(&$input, $offset, $length, $replacement) {
+        $replacement = (array) $replacement;
+        $key_indices = array_flip(array_keys($input));
+        if (isset($input[$offset]) && is_string($offset)) {
+                $offset = $key_indices[$offset];
+        }
+        if (isset($input[$length]) && is_string($length)) {
+                $length = $key_indices[$length] - $offset;
+        }
+
+        $input = array_slice($input, 0, $offset, TRUE)
+                + $replacement
+                + array_slice($input, $offset + $length, NULL, TRUE);
+}
+
