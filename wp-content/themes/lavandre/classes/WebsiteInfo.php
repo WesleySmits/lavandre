5a84b3d3aa25a110e2239f47a3158d95fa848e74
Wesley Smits <wesley@intermation.nl>
2022-02-28 20:43:41 +0100

    Added .at / .lu / .ch domains to language switcher

--- wp-content/themes/lavandre/classes/WebsiteInfo.php
+++ wp-content/themes/lavandre/classes/WebsiteInfo.php
@@ -6,19 +6,27 @@ class WebsiteInfo
 {
     public $websiteInfo = [
         'International' => 'International',
+        'Österreich' => 'Österreich',
         'België' => 'België',
         'Belgique' => 'Belgique',
         'Deutschland' => 'Deutschland',
         'France' => 'France',
-        'Nederland' => 'Nederland'
+        'Luxembourg' => 'Luxembourg',
+        'Nederland' => 'Nederland',
+        'Schweiz' => 'Schweiz',
+        'Suisse' => 'Suisse',
     ];
 
     public $websiteInfoMapping = [
         'International' => 'https://lavandre.com/',
+        'Österreich' => 'https://lavandre.de/',
         'België' => 'https://lavandre.nl/',
         'Belgique' => 'https://lavandre.fr/',
         'Deutschland' => 'https://lavandre.de/',
         'France' => 'https://lavandre.fr/',
-        'Nederland' => 'https://lavandre.nl/'
+        'Luxembourg' => 'https://lavandre.de/',
+        'Nederland' => 'https://lavandre.nl/',
+        'Schweiz' => 'https://lavandre.de/',
+        'Suisse' => 'https://lavandre.fr/',
     ];
 }
