e6b991efc23d7d9893d248639e9a605c478bead9
Wesley Smits <wesley@intermation.nl>
2022-03-14 20:27:13 +0100

    Thank you page for subscription

--- wp-content/themes/lavandre/woocommerce/checkout/thankyou.php
+++ wp-content/themes/lavandre/woocommerce/checkout/thankyou.php
@@ -18,9 +18,6 @@
 defined( 'ABSPATH' ) || exit;
 
 $customer_id = $order->get_customer_id();
-$$subscription = wcs_get_subscriptions_for_order( $order );
-
-var_dump($subscription); die;
 
 $addresses = apply_filters(
     'woocommerce_my_account_get_addresses',
